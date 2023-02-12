a173cffdb99b5d18f5c0613facc8b03954632727
alondmnt <alondmnt@users.noreply.github.com>
2023-02-12 23:20:23 +0200

    new: automatically adjust query and re-send when pressing OK in error dialog

--- src/openai.ts
+++ src/openai.ts
@@ -2,11 +2,12 @@ import joplin from 'api';
 import { JarvisSettings } from './settings';
 
 
-export async function query_completion(prompt: string, settings: JarvisSettings) {
+export async function query_completion(
+    prompt: string, settings: JarvisSettings, adjust_max_tokens: number = 0) {
   const responseParams = {
     prompt: prompt,
     model: settings.model,
-    max_tokens: settings.max_tokens - Math.ceil(prompt.length / 4),
+    max_tokens: settings.max_tokens - Math.ceil(prompt.length / 4) - adjust_max_tokens,
     temperature: settings.temperature,
     top_p: settings.top_p,
     frequency_penalty: settings.frequency_penalty,
@@ -22,12 +23,32 @@ export async function query_completion(prompt: string, settings: JarvisSettings)
   });
   const data = await response.json();
 
-  // handle errors
-  if (data.choices == undefined) {
-    await joplin.views.dialogs.showMessageBox('Error:' + data.error.message);
+  // output completion
+  if (data.choices) {
+    return data.choices[0].text;
+  }
+
+  // display error message
+  const errorHandler = await joplin.views.dialogs.showMessageBox('Error: ' + data.error.message);
+
+  // cancel button
+  if (errorHandler == 1) {
     return '';
   }
-  return data.choices[0].text;
+
+  // find all numbers in error message
+  const max_tokens = [...data.error.message.matchAll(/([0-9]+)/g)];
+  console.log(max_tokens);
+
+  // adjust max tokens
+  if ((max_tokens != null) &&
+      (data.error.message.includes('reduce your prompt'))) {
+    adjust_max_tokens = parseInt(max_tokens[1]) - parseInt(max_tokens[0]);
+    console.log(adjust_max_tokens);
+  }
+
+  // retry
+  return await query_completion(prompt, settings, adjust_max_tokens);
 }
 
 export async function query_edit(input: string, instruction: string, settings: JarvisSettings) {
