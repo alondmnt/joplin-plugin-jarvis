322e697bb866be11e3e331fe1ecf601f82ffb552
alondmnt <alondmnt@users.noreply.github.com>
2023-03-04 10:26:01 +0200

    changed: default paper space=100

--- src/jarvis.ts
+++ src/jarvis.ts
@@ -150,7 +150,7 @@ await joplin.views.dialogs.setHtml(dialogHandle, `
     </div>
     <div>
       <label for="n_papers">Paper space</label>
-      <input type="range" title="Search for 20 papers to sample from" name="n_papers" id="n_papers" size="25" min="0" max="500" value="20" step="10"
+      <input type="range" title="Search for 100 papers to sample from" name="n_papers" id="n_papers" size="25" min="0" max="500" value="100" step="10"
        oninput="title='Search for ' + value + ' papers to sample from'" />
     </div>
     <div>
